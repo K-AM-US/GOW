c9618160e7f040aa081520fd5f4e9cc95285a1fc
mauriciocasillas <kamusgallagher@gmail.com>
2023-08-07 19:31:16 -0600

    WeaponController updated.

--- GOW/Controller/WeaponsController.swift
+++ GOW/Controller/WeaponsController.swift
@@ -90,22 +90,12 @@ class WeaponsController: UIViewController, UITableViewDelegate, UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = WeaponCell()
         
-        if self.restorationIdentifier == "cogUi"{
-            cell = tableView.dequeueReusableCell(withIdentifier: "cell1") as! WeaponCell
-            cell.wName.text = NSLocalizedString(arrayWeapons[indexPath.row].name, comment: "")
-            cell.wImage.image = UIImage(named: arrayWeapons[indexPath.row].poster)
-            cell.wDescription.text = NSLocalizedString(arrayWeapons[indexPath.row].description, comment: "")
-        }else{
-            cell = tableView.dequeueReusableCell(withIdentifier: "cell1") as! WeaponCell
-            cell.wName.text = NSLocalizedString(arrayWeapons[indexPath.row].name, comment: "")
-            cell.wImage.image = UIImage(named: arrayWeapons[indexPath.row].poster)
-            cell.wDescription.text = NSLocalizedString(arrayWeapons[indexPath.row].description, comment: "")
-        }
-        
-        
-        
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1") as! WeaponCell
+        cell.wName.text = NSLocalizedString(arrayWeapons[indexPath.row].name, comment: "")
+        cell.wImage.image = UIImage(named: arrayWeapons[indexPath.row].poster)
+        cell.wDescription.text = NSLocalizedString(arrayWeapons[indexPath.row].description, comment: "")
+                
+    return cell
     }
 }
